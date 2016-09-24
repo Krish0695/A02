13518bf961d2fc690d484b4db856d9e776e91d89
Krish0695 <S528152@mail.nwmissouri.edu>
2016-09-23 23:29:26 -0500

    Add files via upload

---
+++ A02Mudduluru/A02.js
@@ -0,0 +1,8 @@
+function myFunction() {
+
+    var x = document.getElementById("mySelect").value;
+    var y = parseInt(x)*5;
+    var z = document.getElementById("movie").value;
+    document.getElementById("demo").value = y;
+   
+}
